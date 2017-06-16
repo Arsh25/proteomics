4a4c6252cc333772ff570680807267bd973e057e
Arsh Chauhan <achauhan@alaska.edu>
2017-06-16 16:03:32 -0400

    Added function to write each scan to it's own csv file

--- main.py
+++ main.py
@@ -8,6 +8,8 @@
 
 import mzxml_parser
 import handle_csv
+import csv
+import pathlib
 
 def colate_scan_info(mzxml_file,scans_file):
 	scan_nums = handle_csv.read_scans_list(scans_file)
@@ -18,10 +20,20 @@ def colate_scan_info(mzxml_file,scans_file):
 		current_scan['scan'] = scan
 		current_scan['peaks'] = mz
 		peptides = handle_csv.get_peptide('real.csv',[scan])
-		current_scan['peptides'] = peptides[0]['peptide'] #list (size 1) of dicts
+		current_scan['peptide'] = peptides[0]['peptide'] #list (size 1) of dicts
 		results.append(current_scan)
 	return results
 
+def write_csv_files(scan_results):
+	pathlib.Path('results').mkdir(parents=True,exist_ok=True)
+	field_names=['peptide','peaks']
+	for scan in scan_results:
+		with open('results/'+str(scan['scan'])+'.csv','w') as csv_file:
+			csv_writer = csv.DictWriter(csv_file,fieldnames=field_names,dialect='excel')
+			csv_writer.writeheader()
+			csv_writer.writerow({'peptide':scan['peptide'],'peaks':scan['peaks']})
+
+
 if __name__ == '__main__':
 	collated_data = colate_scan_info('real.mzXML','scan_list.csv')
-	print(collated_data)
+	write_csv_files(collated_data)
