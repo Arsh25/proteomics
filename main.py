6141cc7339abe5c4f13522478e430e59f7fbe7d1
Arsh Chauhan <achauhan@alaska.edu>
2017-06-20 14:17:31 -0400

    write_csv_files now makes msviewer compatible results files

--- main.py
+++ main.py
@@ -18,25 +18,28 @@ def colate_scan_info(mzxml_file,scans_file):
 	results = []
 	for scan in scan_nums:
 		current_scan ={}
-		mz,mz_intensity = mzxml_parser.get_peaks(mzxml_file,scan)
+		mz,mz_intensity,precursor_charge = mzxml_parser.get_peaks(mzxml_file,scan)
 		current_scan['scan'] = scan
 		current_scan['peaks'] = mz
 		peptides = handle_csv.get_peptide('real.csv',[scan])
 		current_scan['peptide'] = peptides[0]['peptide'] #list (size 1) of dicts
+		current_scan['precursor_mz'] = peptides[0]['precursor_mz']
+		current_scan['mods'] = peptides[0]['mods']
 		results.append(current_scan)
 	return results
 
-def write_csv_files(experiment,scan_results):
+def write_csv_files(experiment,scan_results,peaks=True):
 	pathlib.Path('results/'+experiment).mkdir(parents=True,exist_ok=True)
-	field_names=['peptide','peaks']
+	field_names=['peptide','mods','scan','peaks','precursor_mz']
 	os.chdir('results/'+experiment)
 	for scan in scan_results:
 		with open(str(scan['scan'])+'.csv','w') as csv_file:
 			csv_writer = csv.DictWriter(csv_file,fieldnames=field_names,dialect='excel')
 			csv_writer.writeheader()
-			csv_writer.writerow({'peptide':scan['peptide']})
-			for peak in zip(scan['peaks']):# Write each peak in new row
-				csv_writer.writerow({'peaks':peak[0]})
+			csv_writer.writerow({'peptide':scan['peptide'],'mods':scan['mods'],'scan':scan['scan'],'precursor_mz':scan['precursor_mz']})
+			if peaks:
+				for peak in zip(scan['peaks']):# Write each peak in new row
+					csv_writer.writerow({'peaks':peak[0]})
 
 
 if __name__ == '__main__':
