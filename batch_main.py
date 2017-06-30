8afa45f06f5c749886f94410eeeb605d6f78d99a
Arsh Chauhan <achauhan@alaska.edu>
2017-06-30 13:54:14 -0400

    Added batch zip job

--- batch_main.py
+++ batch_main.py
@@ -3,7 +3,7 @@
 # Arsh Chauhan
 # batch_main.py: Run main.py in batch mode
 # 06/26/2017
-# Last Edited: 06/26/2017
+# Last Edited: 06/27/2017
 # MIT License (2017)
 import subprocess
 import os
@@ -19,10 +19,30 @@ def run_main(directory,excluded_files):
 				with open('auto_run.log','a') as log:
 					log.write('running for '+ filename + ' existed with ' + str(process.returncode) + '\n')
 
+def zip_results(results_dir):
+	for folder in os.walk(results_dir):
+		if folder[0].find('.mzXML') != -1:
+			experiment_name = folder[0].find('.mzXML')
+			print ("zipping folder "+ folder[0])
+			process = subprocess.run(["/usr/bin/zip","-r",folder[0][:experiment_name]+".zip",folder[0]+"/"])
+			if process.returncode != 0:
+				with open('auto_run.log','a') as log:
+					log.write('zipping '+ folder[0] + ' existed with ' + str(process.returncode) + '\n')
+def create_do_not_check_list(results_dir):
+	for folder in os.walk(results_dir):
+		if folder[0].find('.mzXML') != -1:
+			print ("Processing results from "+ folder[0])
+			process = subprocess.run(["./filter.py","--pfilter",folder[0]])
+			if process.returncode != 0:
+				with open('auto_run.log','a') as log:
+					log.write('processing '+ folder[0] + ' existed with ' + str(process.returncode) + '\n')
+
 if __name__ == '__main__':
 	confirm = input("Are you sure you want to run this batch job (Y/N?)")
 	if confirm.lower() == "y":
-		completed_files = ['160108_C1021-2_a.mzXML','160108_C1021-2_c.mzXML','160108_C812-1_b.mzXML','160108_C812-1_c.mzXML','160108_C923-1_a.mzXML','160108_I122-1-K_a.mzXML']
-		run_main("/media/arsh/ResearchRazzle/converted",completed_files)
+		# completed_files = ['160108_C1021-2_a.mzXML','160108_C1021-2_c.mzXML','160108_C812-1_b.mzXML','160108_C812-1_c.mzXML','160108_C923-1_a.mzXML','160108_I122-1-K_a.mzXML']
+		# run_main("/media/arsh/ResearchRazzle/converted",completed_files)
+		#zip_results("results")
+		create_do_not_check_list("results")
 	else:
 		print("Exiting")
