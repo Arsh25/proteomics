3a09c78191efb51c319ba8825d28f2959c80c928
Arsh Chauhan <achauhan@alaska.edu>
2017-06-30 13:55:22 -0400

    Added fileter by peak:gives a csv files with all psm results that return a peak between two values

--- filter.py
+++ filter.py
@@ -5,6 +5,7 @@ from handle_csv import write_to_csv
 import csv
 import os
 import pathlib
+import argparse
 
 def sort_by_peptide(filename):
 	pathlib.Path('results/peptides').mkdir(parents=True,exist_ok=True)
@@ -40,9 +41,47 @@ def min_fdr(folder):
 			min_fdr.append(reader[0])
 	return min_fdr
 
+def filter_by_peaks(results_dir,min_val,max_val):
+	min_val = str(min_val)
+	max_val = str(max_val)
+	fieldnames = ['spectra_set','scan,precursor_neutral_mass','calc_neutral_pep_mass',\
+				'peptide','mods','protein','estfdr','peaks']
+	base_path = os.path.dirname(os.path.realpath(__file__))
+	good_psm_list =[]
+	for file in os.listdir(results_dir):
+		if file.find('.zip') != 0:
+			print("Processing: " +file)
+			with open(results_dir+"/"+file,'r') as results_file:
+				dialect = csv.Sniffer().sniff(results_file.read(1024))
+				results_file.seek(0)
+				reader = csv.DictReader(results_file,dialect=dialect)
+				spectra_set = ""
+				scan = ""
+				for row in reader:
+					if row['spectra_set'] != "":
+						spectra_set = row['spectra_set']
+					if row['scan'] != "":
+						scan = row['scan']
+					if row['peaks'] >= min_val and row['peaks'] <= max_val:
+			 			good_psm = {}
+			 			good_psm['spectra_set'] = spectra_set
+			 			good_psm['scan'] = scan
+			 			good_psm['peak'] = row['peaks']
+			 			good_psm_list.append(good_psm)
+	os.chdir(base_path+"/results")
+	with open('do_not_check.csv','a') as output_file:
+		writer = csv.DictWriter(output_file,fieldnames=['spectra_set','scan','peak'])
+		writer.writeheader()
+		for spectra_set in good_psm_list:
+			writer.writerow(spectra_set)
+
 if __name__ == '__main__':
 	#sort_by_peptide('remaining_psm.csv')
-	min_fdr('results/peptides')
-	write_to_csv('min_fdr',min_fdr('results/peptides'))
-
+	#min_fdr('results/peptides')
+	#write_to_csv('min_fdr',min_fdr('results/peptides'))
+	parser = argparse.ArgumentParser(description='Various functions to filter results')
+	parser.add_argument('--pfilter',type=str,help='check scans from this results \
+						folder for peaks between 259.50 and 260.77',)
+	args = parser.parse_args()
+	filter_by_peaks(args.pfilter,259.50,260.77)
 
