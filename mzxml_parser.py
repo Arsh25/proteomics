4e256e5d604c20bc67cfa34aeb9da4e4374b6f6a
Arsh Chauhan <achauhan@alaska.edu>
2017-07-02 11:15:50 -0400

    try block around read

--- mzxml_parser.py
+++ mzxml_parser.py
@@ -21,20 +21,53 @@ def find_scan(file_name,scan_num):
 				current_scan = line[line.find("=")+2:-2]
 				if current_scan == str(scan_num):
 					while(True):
-						next_line = scan_file.readline()
-						previous_line = next_line
-						if next_line.find('<precursorMz') !=-1:
-							end_tag = next_line.find('>')
-							precursor_charge = next_line[end_tag+1:next_line.find('</precursorMz>')]
-						elif next_line.find('<peaks') !=-1:
-							output = ""
-							while(True):
-								next_line = scan_file.readline()
-								output += next_line
-								if next_line.find(">") != -1:
-									peaks = output[:output.find("<")]
-									return peaks,precursor_charge
+						try:
+							next_line = scan_file.readline()
+							previous_line = next_line
+							if next_line.find('<precursorMz') !=-1:
+								end_tag = next_line.find('>')
+								precursor_charge = next_line[end_tag+1:next_line.find('</precursorMz>')]
+							elif next_line.find('<peaks') !=-1:
+								output = ""
+								while(True):
+									next_line = scan_file.readline()
+									output += next_line
+									if next_line.find(">") != -1:
+										peaks = output[:output.find("<")]
+										return peaks,precursor_charge
+						except:
+							print ("Could not handle scan: "+current_scan)
 
+def find_scan_RAM(file_name,scan_nums_list):
+	with open (file_name,'r') as scan_file:
+		if scan_file is None:
+			print (file_name)
+		peaks = []
+		precursor_charge = []
+		memory_file = []
+		for line in scan_file:
+			memory_file.append(line)
+		for scan in scan_nums_list:
+			for line in memory_file:
+				if line.find("<scan") != -1:
+					current_scan = line[line.find("=")+2:-2]
+					if str(scan) == current_scan:
+						while(True):
+							next_line = scan_file.readline()
+							previous_line = next_line
+							if next_line.find('<precursorMz') !=-1:
+								end_tag = next_line.find('>')
+								current_precursor_charge = next_line[end_tag+1:next_line.find('</precursorMz>')]
+							elif next_line.find('<peaks') !=-1:
+								output = ""
+								while(True):
+									next_line = scan_file.readline()
+									output += next_line
+									if next_line.find(">") != -1:
+										current_peak = output[:output.find("<")]
+										peaks.append(current_peak)
+										precursor_charge.append(current_precursor_charge)
+		return peaks,precursor_charge
 									
 def parse_peaks(peaks_decoded):
 	#Based on code by Taejoon Kwon (https://code.google.com/archive/p/massspec-toolbox/)
@@ -54,11 +87,21 @@ def parse_peaks(peaks_decoded):
 		idx += 1
 	return mz_list,intensity_list
 
-def get_peaks(mzxml_file,scan):
+def get_peaks(mzxml_file,scan,RAM=False):
 	scan_data,precursor_charge = find_scan(mzxml_file,scan)
 	encoded_peaks = scan_data[scan_data.find(">")+1:]
 	peaks = base64.b64decode(encoded_peaks)
 	mz_list,intensity_list = parse_peaks(peaks)
+	return mz_list,intensity_list,precursor_charge
+
+def get_peaks_RAM(mzxml_file,scan_list):
+	scan_data,precursor_charge = find_scan_RAM(mzxml_file,scan_list)
+	all_mz = []
+	for scan in scan_data:
+		encoded_peaks = scan_data[scan_data.find(">")+1:]
+		peaks = base64.b64decode(encoded_peaks)
+		mz_list,intensity_list = parse_peaks(peaks)
+		all_mz.append()
 	return mz_list,intensity_list,precursor_charge
 
 def write_to_csv(filename,data):
@@ -69,12 +112,13 @@ def write_to_csv(filename,data):
 
 if __name__ == '__main__':
 	#data = find_scan("real.mzXML",5000)	
-	mz_list,intensity_list,precursor_charge = get_peaks('real.mzXML',14)
-
+		#mz_list,intensity_list,precursor_charge = get_peaks('160108_C812-1_a.mzXML',21822)
+	peaks,precursor_charge = find_scan('160108_C812-1_a.mzXML',21822)
 	print (precursor_charge)
-	print('{0:15} | {1:2}'.format("        mz","mz intensity"))
-	for mz,intensity in zip(mz_list,intensity_list):
-		print('{0:15f} | {1:2f}'.format(mz,intensity))
+	print (peaks)
+	# print('{0:15} | {1:2}'.format("        mz","mz intensity"))
+	# for mz,intensity in zip(mz_list,intensity_list):
+	# 	print('{0:15f} | {1:2f}'.format(mz,intensity))
 
-	print("GENERATING csv file. Open output.csv after termination")
-	write_to_csv("output",mz_list)
+	# print("GENERATING csv file. Open output.csv after termination")
+	# write_to_csv("output",mz_list)
